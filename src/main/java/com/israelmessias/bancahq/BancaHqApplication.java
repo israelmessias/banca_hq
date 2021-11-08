39e183e80f38f8e49203564d88a996a7b9218bed
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-11-08 17:11:09 -0300

    iniciando projeto hq

---
+++ src/main/java/com/israelmessias/bancahq/BancaHqApplication.java
@@ -0,0 +1,13 @@
+package com.israelmessias.bancahq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancaHqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancaHqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/israelmessias/bancahq/BancaHqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelmessias.bancahq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancaHqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
