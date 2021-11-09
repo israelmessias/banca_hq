dffa2f2c3d1f6905e14e37ef96afe107f58c0269
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-11-09 14:30:43 -0300

    Serviço Hq implementado

--- src/main/java/com/israelmessias/bancahq/service/HqService.java
+++ src/main/java/com/israelmessias/bancahq/service/HqService.java
@@ -1,2 +1,17 @@
-package com.israelmessias.bancahq.service;public interface HqService {
+package com.israelmessias.bancahq.service;
+
+import com.israelmessias.bancahq.model.entity.Hq;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface HqService {
+
+    Hq salvarHq(Hq hq);
+
+    void excluirHq(Hq hq);
+
+    List<Hq> buscarHq(Hq Hq);
+
+    Optional<Hq> obterPorId(Long id);
 }
--- src/main/java/com/israelmessias/bancahq/service/impl/HqServiceImpl.java
+++ src/main/java/com/israelmessias/bancahq/service/impl/HqServiceImpl.java
@@ -1,2 +1,68 @@
-package com.israelmessias.bancahq.service.impl;public class HqServiceImpl {
+package com.israelmessias.bancahq.service.impl;
+
+import com.israelmessias.bancahq.excecao.RegraDeNegocio;
+import com.israelmessias.bancahq.model.entity.Hq;
+import com.israelmessias.bancahq.model.repository.HqRepository;
+import com.israelmessias.bancahq.service.HqService;
+import org.springframework.data.domain.Example;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.ExampleMatcher;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class HqServiceImpl implements HqService {
+
+    @Autowired
+    private HqRepository repository;
+
+    @Override
+    @Transactional
+    public Hq salvarHq(Hq hq) {
+        validar(hq);
+        return repository.save(hq);
+    }
+
+    @Override
+    @Transactional
+    public void excluirHq(Hq hq) {
+        Objects.requireNonNull(hq.getId());
+        repository.delete(hq);
+    }
+
+    @Override
+    @Transactional()
+    public List<Hq> buscarHq(Hq hq) {
+        Example<Hq> example = Example.of(hq, ExampleMatcher.matching()
+                .withIgnoreCase().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+        return repository.findAll(example);
+    }
+
+    @Override
+    public Optional<Hq> obterPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    private void validar(Hq hq)
+    {
+        if(hq.getNome() == null|| hq.getNome().trim().equals(" "))
+        {
+            throw new RegraDeNegocio("Informe nome valido!");
+        }
+        if(hq.getGenero() == null || hq.getGenero().trim().equals(" "))
+        {
+            throw new RegraDeNegocio("Informe genero valido!");
+        }
+        if(hq.getEditora() == null)
+        {
+            throw new RegraDeNegocio("Informe a Editora!");
+        }
+        if(hq.getPreco() == null|| hq.getPreco().compareTo(BigDecimal.ZERO.doubleValue()) < 1)
+        {
+            throw new RegraDeNegocio("Informe o valor!");
+        }
+    }
 }
